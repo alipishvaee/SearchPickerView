fa6e81d9048846d796109c2b1a0465b97ddae8ac
Ali Pishvaee <alipishvaee2@Gmail.com>
2021-02-07 02:29:48 +0330

    Initial Commit

---
+++ SearchPickerView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchPickerView
+//
+//  Created by Ali Pishvaee on 2/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
